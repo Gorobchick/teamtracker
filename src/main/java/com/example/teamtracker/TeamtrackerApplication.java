619a919721d7c70541c582d226face56bc51b1c9
Gorobchick <o.cropiva@gmail.com>
2023-10-23 17:05:33 +0300

    Main

---
+++ src/main/java/com/example/teamtracker/TeamtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamtrackerApplication.class, args);
+	}
+
+}
