241b88debcbc96550e3ce3c962f4243f790af75e
Gorobchick <o.cropiva@gmail.com>
2023-10-23 18:01:55 +0300

    Main

--- src/main/java/com/example/teamtracker/controllers/MainController.java
+++ src/main/java/com/example/teamtracker/controllers/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "Головна сторінка");
-        return "Main";
+        return "main";
     }
 
 }
